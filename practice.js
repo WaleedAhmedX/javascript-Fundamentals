0542f15188c3230fa7740c70a4b5e0d20d089cfd
Waleed Ahmed <waleedahmad149300@gmail.com>
2026-07-07 12:18:56 +0500

    Modern Java Script
    Just a modern Javascript file!

---
+++ practice.js
@@ -0,0 +1,32 @@
+let name ="waleed";
+
+name="Waleed ahmed";
+
+console.log(`The name of the Student is: ${name}`);
+
+const number=3008543699;
+
+console.log(`The phone number is: ${number}`);
+
+const greet=(name)=>console.log(`hello ${name}`);
+
+greet(name);
+
+
+/-------------------------------------------------/
+
+const Univeristy="NUML";
+
+let semester=5;
+semester=6;
+
+console.log(`I study at ${Univeristy}`);
+
+console.log(`I am a student of ${semester} semester!`);
+
+const great=(name)=>`Hello! my name is ${name}`;
+
+console.log(great("Waleed Ahmed"));
+
+
+
