b30f8abb675d4790d92446cf8cb7dc01494d51d6
Waleed Ahmed <waleedahmad149300@gmail.com>
2026-07-07 12:44:18 +0500

    Create practice2.js

---
+++ practice2.js
@@ -0,0 +1,28 @@
+const name="Waleed Ahmed";
+
+const Age=25;
+
+const semester=5;
+
+console.log(name+" "+Age+" "+semester);
+
+/----------------------------------/
+
+let student =
+{
+    name: "Waleed Ahmed",
+    age: 26,
+    semester: 5
+
+};
+
+
+console.log(`${student.name}`);
+
+student.university="NUML";
+
+console.log(`Name: ${student.name}
+Age: ${student.age}
+Semester: ${student.semester}
+University: ${student.university}    `);
+
