9b8760b935da93de099040355e6d63dff71ae3dd
Waleed Ahmed <waleedahmad149300@gmail.com>
2026-07-07 18:59:30 +0500

    Update practice2.js

--- practice2.js
+++ practice2.js
@@ -26,3 +26,49 @@ Age: ${student.age}
 Semester: ${student.semester}
 University: ${student.university}    `);
 
+/----------------------------------------/
+
+const arr=["waleed ahmed", 76, "NUML", 398765432];
+
+console.log(arr[0], arr[1]);
+
+arr.push("My name is Waleed Ahmed!");
+
+console.log(arr[4]);
+
+
+/------------------------------------------------/
+
+
+const laptop =
+{
+    ram: "16GB",
+    brand: "HP",
+    storage: "512GB"
+};
+
+laptop.screen="16 inch";
+
+
+console.log(laptop.screen);
+
+console.log(`${laptop.screen}
+ ${laptop.ram}
+ ${laptop.brand}
+ ${laptop.storage}`);
+
+const {ram,brand}=laptop;
+
+console.log(`the Laptops ram is: ${laptop.ram}`);
+
+console.log(ram);
+
+const students=["Name", "Semester"];
+
+console.log(...students);
+
+const newarr=[...students,...arr];
+
+console.log(newarr);
+
+
